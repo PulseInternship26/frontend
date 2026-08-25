0ebce3186c7af135f88cc3772a35e3968759399a
Youssef Nabeel <neblo.2005@gmail.com>
2026-08-26 01:31:05 +0300

    Add page skeletons: signup, login, customer-home, admin-dashboard, manage-admins, navbar, auth service

---
+++ src/app/pages/manage-admins/manage-admins.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ManageAdmins } from './manage-admins';
+
+describe('ManageAdmins', () => {
+  let component: ManageAdmins;
+  let fixture: ComponentFixture<ManageAdmins>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ManageAdmins],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(ManageAdmins);
+    component = fixture.componentInstance;
+    await fixture.whenStable();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/pages/manage-admins/manage-admins.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-manage-admins',
+  imports: [],
+  templateUrl: './manage-admins.html',
+  styleUrl: './manage-admins.css',
+})
+export class ManageAdmins {}
