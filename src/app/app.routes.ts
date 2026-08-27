343e62c91437b3f6650b27e9d268747e8942cded
Youssef Nabeel <neblo.2005@gmail.com>
2026-08-28 01:33:01 +0300

    Add routing: signup, login, home, admin/books, admin/admins routes; clean up app.html to router-outlet only

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { Signup } from './pages/signup/signup';
+import { Login } from './pages/login/login';
+import { CustomerHome } from './pages/customer-home/customer-home';
+import { AdminDashboard } from './pages/admin-dashboard/admin-dashboard';
+import { ManageAdmins } from './pages/manage-admins/manage-admins';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'signup', pathMatch: 'full' },
+  { path: 'signup', component: Signup },
+  { path: 'login', component: Login },
+  { path: 'home', component: CustomerHome },
+  { path: 'admin/books', component: AdminDashboard },
+  { path: 'admin/admins', component: ManageAdmins },
+];
